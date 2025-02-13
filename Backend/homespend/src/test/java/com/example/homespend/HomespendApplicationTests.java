a9360a47e5534765fa4441c7366fce64f3068af3
Oana <andronachebogdan97@yahoo.com>
2025-02-13 16:11:54 +0200

    Spring Initializr

---
+++ Backend/homespend/src/test/java/com/example/homespend/HomespendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homespend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomespendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
